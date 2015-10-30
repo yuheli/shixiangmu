6045805cfd25300b1b92b658a38878448b9437ca
yuheli <ios-04@ACC8084E.ipt.aol.com>
2015-10-30 11:13:45 +0800

    version1.0
    good

---
+++ bbb/bbb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bbb
+//
+//  Created by ios－04 on 15/10/30.
+//  Copyright © 2015年 wwz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ bbb/bbb/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  bbb
+//
+//  Created by ios－04 on 15/10/30.
+//  Copyright © 2015年 wwz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
